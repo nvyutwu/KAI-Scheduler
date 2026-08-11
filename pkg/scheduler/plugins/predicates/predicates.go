976b2b9ef6adb2114369809ff576edcf6426a6d8
davidLif <davidshani12@gmail.com>
2026-08-11 23:50:30 +0300

    Remove unused functions
    Signed-off-by: davidLif <davidshani12@gmail.com>

--- pkg/scheduler/plugins/predicates/predicates.go
+++ pkg/scheduler/plugins/predicates/predicates.go
@@ -22,7 +22,6 @@ package predicates
 import (
 	"fmt"
 	"slices"
-	"strconv"
 	"strings"
 
 	"k8s.io/apimachinery/pkg/util/sets"
@@ -33,11 +32,9 @@ import (
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/api/node_info"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/api/pod_info"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/api/podgroup_info"
-	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/api/resource_info"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/cache/cluster_info"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/conf"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/framework"
-	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/gpu_sharing"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/k8s_internal"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/k8s_internal/predicates"
 	"github.com/kai-scheduler/KAI-scheduler/pkg/scheduler/log"
@@ -389,71 +386,6 @@ func (pp *predicatesPlugin) evaluateTaskOnPredicates(
 	}
 
 	return nil
-}
-
-func (pp *predicatesPlugin) checkMaxPodsWithGpuGroupReservation(
-	task *pod_info.PodInfo, node *node_info.NodeInfo) error {
-	availablePods := node.IdleVector.Get(resource_info.PodsIndex) + node.ReleasingVector.Get(resource_info.PodsIndex)
-
-	if !task.IsSharedGPURequest() {
-		if availablePods > 0 {
-			return nil
-		}
-		return common_info.NewFitError(task.Name, task.Namespace, node.Name, api.NodePodNumberExceeded)
-	}
-
-	needsNewGpuGroup := pp.willCreateNewGpuGroup(task, node)
-	if !needsNewGpuGroup {
-		return nil
-	}
-
-	if availablePods < 2 {
-		return common_info.NewFitError(task.Name, task.Namespace, node.Name, api.NodePodNumberExceeded)
-	}
-
-	return nil
-}
-
-// willCreateNewGpuGroup determines if allocating this task will create a new GPU group
-// (and thus require a new reservation pod).
-func (pp *predicatesPlugin) willCreateNewGpuGroup(task *pod_info.PodInfo, node *node_info.NodeInfo) bool {
-	if pp.ssn == nil {
-		return true
-	}
-
-	fittingGPUs := pp.ssn.FittingGPUs(node, task)
-	gpuForSharingImmediate := gpu_sharing.GetNodePreferableGpuForSharing(fittingGPUs, node, task, false)
-
-	if gpuForSharingImmediate != nil && !gpuForSharingImmediate.IsReleasing {
-		return containsNewGpuGroup(gpuForSharingImmediate.Groups)
-	}
-
-	gpuForSharingPipelined := gpu_sharing.GetNodePreferableGpuForSharing(fittingGPUs, node, task, true)
-
-	if gpuForSharingPipelined != nil {
-		return containsNewGpuGroup(gpuForSharingPipelined.Groups)
-	}
-
-	// No GPU assignment possible - conservatively assume new group would be needed
-	return true
-}
-
-// containsNewGpuGroup checks if any of the GPU groups is a newly created one (UUID format).
-func containsNewGpuGroup(groups []string) bool {
-	for _, gpuGroup := range groups {
-		if isNewGpuGroup(gpuGroup) {
-			return true
-		}
-	}
-	return false
-}
-
-// isNewGpuGroup determines if a GPU group ID represents a new group (UUID) vs an existing one (numeric).
-func isNewGpuGroup(gpuGroup string) bool {
-	// New GPU groups are UUIDs (e.g., "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx")
-	// Existing GPU groups are numeric strings ("0", "1", "2", etc.)
-	_, err := strconv.Atoi(gpuGroup)
-	return err != nil // If not a number, it's a UUID = new group
 }
 
 func (pp *predicatesPlugin) OnSessionClose(_ *framework.Session) {}
